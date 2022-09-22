1b616c0d3540ed58eee2d3378e43ff77e2579b18
= <=>
2022-09-22 19:01:05 -0300

    Mudando a construção do cubo

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var cameraRotation = [180, 180, 0];
 var cameraLocation;
 var numElements = 36;
 var vertices = [
-    vec3(-0.5, -0.5,  0.5),
-    vec3(-0.5,  0.5,  0.5),
-    vec3(0.5,  0.5,  0.5),
-    vec3(0.5, -0.5,  0.5),
-    vec3(-0.5, -0.5, -0.5),
+    vec3( 0.5,  0.5, 0.5),
+    vec3(-0.5,  0.5, 0.5),
+    vec3( 0.5, -0.5, 0.5),
+    vec3(-0.5, -0.5, 0.5),
+    vec3( 0.5,  0.5, -0.5),
     vec3(-0.5,  0.5, -0.5),
-    vec3(0.5,  0.5, -0.5),
-    vec3(0.5, -0.5, -0.5)
+    vec3( 0.5, -0.5, -0.5),
+    vec3(-0.5, -0.5, -0.5),
 ];
 var vertexColors = [
     vec4(0.0, 0.0, 0.0, 1.0),  // black
@@ -29,18 +29,18 @@ var vertexColors = [
 
 // indices of the 12 triangles that compise the cube
 var indices = [
-    1, 0, 3,
-    3, 2, 1,
-    2, 3, 7,
-    7, 6, 2,
-    3, 0, 4,
-    4, 7, 3,
-    6, 5, 1,
-    1, 2, 6,
-    4, 5, 6,
-    6, 7, 4,
-    5, 4, 0,
-    0, 1, 5
+    2, 1, 0, //face da frente
+    2, 1, 3, //face da frente
+    3, 6, 2, //face de cima
+    3, 6, 7, //face de cima
+    6, 5, 4, //face de trás
+    6, 5, 7, //face de trás
+    4, 1, 0, //face de baixo
+    4, 1, 5, //face de baixo
+    6, 0, 2, //face da esquerda em relação a frente
+    6, 0, 4, //face da esquerda em relação a frente
+    7, 1, 3, //face da direita em relação a frente
+    7, 1, 5  //face da direita em relação a frente
 ];
 
 window.onload = function init(){
@@ -67,14 +67,14 @@ window.onload = function init(){
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
 
     var colorLoc = gl.getAttribLocation(program, "aColor");
-    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, true, 0, 0);
     gl.enableVertexAttribArray(colorLoc);
 
     // vertex array attribute buffer
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
-
+    
     var positionLoc = gl.getAttribLocation( program, "aPosition");
     gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc );
@@ -99,7 +99,6 @@ window.onload = function init(){
     };
     const cameraX = document.getElementById( "cameraX" );
     const cameraY = document.getElementById( "cameraY" );
-
     cameraX.onmousemove = function () {
         cameraRotation[0] = cameraX.value;
     };
@@ -110,7 +109,7 @@ window.onload = function init(){
 }
 
 function render(){
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+   // gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.uniform3fv(cameraLocation, cameraRotation);
 
     gl.drawElements(gl.TRIANGLES, numElements, gl.UNSIGNED_BYTE, 0);
