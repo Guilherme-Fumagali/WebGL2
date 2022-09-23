87d36c146fc81b002adf88eccc887dd66a3626f7
Rodrigo Amaral <rodrigohamral@gmail.com>
2022-09-22 21:04:14 -0300

    Forma final

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ var cameraLocation;
 var flag = true;
 var numElements = 36;
 var vertices = [
-    vec3(-0.5, -0.1,  0.5),
-    vec3(-0.5,  0.1,  0.5),
-    vec3(0.5,  0.1,  0.5),
-    vec3(0.5, -0.1,  0.5),
-    vec3(-0.5, -0.1, -0.5),
-    vec3(-0.5,  0.1, -0.5),
-    vec3(0.5,  0.1, -0.5),
-    vec3(0.5, -0.1, -0.5)
+    vec3(0.1, -0.2,  0.1),
+    vec3(0.1,  0.2,  0.1),
+    vec3(0.5,  0.2,  0.1),
+    vec3(0.5, -0.2,  0.1),
+    vec3(-0.5, -0.2, -0.5),
+    vec3(-0.5,  0.2, -0.5),
+    vec3(0.5,  0.5, -0.5),
+    vec3(0.5, -0.5, -0.5)
 ];
 var vertexColors = [
-    vec4(0.0, 0.1, 0.0, 1.0),  // black
+    vec4(0.5, 0.1, 0.0, 1.0),  // black
     vec4(0.0, 0.2, 0.0, 1.0),  // red
-    vec4(0.0, 0.3, 0.0, 1.0),  // yellow
-    vec4(0.0, 0.4, 0.0, 1.0),  // green
-    vec4(0.0, 0.5, 0.0, 1.0),  // blue
-    vec4(0.0, 0.6, 0.0, 1.0),  // magenta
-    vec4(0.0, 0.7, 0.0, 1.0),  // white
-    vec4(0.0, 0.8, 0.0, 1.0)   // cyan
+    vec4(0.0, 0.3, 0.5, 1.0),  // yellow
+    vec4(0.1, 0.4, 0.1, 1.0),  // green
+    vec4(0.5, 0.5, 0.0, 1.0),  // blue
+    vec4(0.9, 0.6, 0.2, 1.0),  // magenta
+    vec4(0.0, 0.7, 0.5, 1.0),  // white
+    vec4(0.5, 0.8, 0.5, 1.0)   // cyan
 ];
 
 // indices of the 12 triangles that compise the cube
@@ -58,6 +58,7 @@ window.onload = function init(){
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
+    gl.enable(gl.DEPTH_TEST);;
 
     //  Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
